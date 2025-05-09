53d974e06bfe6b81d86de75d5b5d71599d305d83
Eugene Zh <storonaot@gmail.com>
2025-05-09 21:34:21 +0300

    Add linter and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "no-debugger": "warn",
+  },
+};
